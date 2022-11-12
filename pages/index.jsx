1402f7773b14e0aaf9442965c50645fe9591a0b4
Trenton Rogers <trent@MacBook-Pro.local>
2022-11-11 22:14:10 -0600

    feat: update copy, bg color, and link opening

--- pages/index.jsx
+++ pages/index.jsx
@@ -10,19 +10,19 @@ export default function HomePage() {
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <div class="h-screen flex flex-col justify-center items-center bg-slate-800">
+      <div class="h-screen flex flex-col justify-center items-center bg-slate-900">
         <div class="max-w-xl px-4">
           <h1 class="text-slate-300 text-4xl font-bold tracking-wide mb-2">Trenton Rogers</h1>
-          <p class="text-slate-200 text-xl mb-4">I am a software developer based out of Milwaukee, Wisconsin.</p>
+          <p class="text-slate-200 text-lg mb-4">I am a software developer based out of Milwaukee, Wisconsin. I enjoy solving challenging problems and exploring new technologies, and I'm always striving to become a stronger developer than I was the day before.</p>
           <div class="flex justify-center gap-10">
-            <a href="https://github.com/trentkrogers">
+            <a href="https://github.com/trentkrogers" target="_blank" rel="noopener noreferrer">
               <Image
                 src="/images/GitHub-Mark-Light-64px.png"
                 height={32}
                 width={32}
                 alt="GitHub" />
             </a>
-            <a href="https://www.linkedin.com/in/trentonrogers/">
+            <a href="https://www.linkedin.com/in/trentonrogers/" target="_blank" rel="noopener noreferrer">
               <Image
                 src="/images/In-White-48@2x.png"
                 height={32}
