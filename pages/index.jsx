5b122b89ed22c9b867ed4a9e8544dee7d92cba61
Trenton Rogers <trent@MacBook-Pro.local>
2022-11-12 11:07:53 -0600

    fix: class -> className

--- pages/index.jsx
+++ pages/index.jsx
@@ -10,11 +10,11 @@ export default function HomePage() {
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <div class="h-screen flex flex-col justify-center items-center bg-slate-800">
-        <div class="max-w-xl px-4">
-          <h1 class="flex justify-center text-white text-4xl font-bold tracking-wide mb-2">Trenton Rogers</h1>
-          <p class="text-slate-200 text-lg mb-4">I am a software developer based out of Milwaukee, Wisconsin. I enjoy solving challenging problems and exploring new technologies, and I'm always striving to become a stronger developer than I was the day before.</p>
-          <div class="flex justify-center gap-10">
+      <div className="h-screen flex flex-col justify-center items-center bg-slate-800">
+        <div className="max-w-xl px-4">
+          <h1 className="flex justify-center text-white text-4xl font-bold tracking-wide mb-2">Trenton Rogers</h1>
+          <p className="text-slate-200 text-lg mb-4">I am a software developer based out of Milwaukee, Wisconsin. I enjoy solving challenging problems and exploring new technologies, and I'm always striving to become a stronger developer than I was the day before.</p>
+          <div className="flex justify-center gap-10">
             <a href="https://github.com/trentkrogers" target="_blank" rel="noopener noreferrer">
               <Image
                 src="/images/GitHub-Mark-Light-64px.png"
