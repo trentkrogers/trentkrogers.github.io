825ead6ddacc8e8c101241ac7cfa0686c8855757
Trenton Rogers <trent@MacBook-Pro.local>
2022-11-12 12:00:00 -0600

    fix: comment

--- next.config.js
+++ next.config.js
@@ -7,8 +7,8 @@ if (isGithubActions) {
     // trim off `<owner>/`
     const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
 
-    assetPrefix = `/${repo}/`
-    basePath = `/${repo}`
+    // assetPrefix = `/${repo}/`
+    // basePath = `/${repo}`
 }
 
 module.exports = {
