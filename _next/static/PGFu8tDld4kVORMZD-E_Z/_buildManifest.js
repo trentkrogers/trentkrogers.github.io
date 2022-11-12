7a293ffc8d085892cd1a122f4710277e8d916b9c
trentkrogers <16307439+trentkrogers@users.noreply.github.com>
2022-11-12 17:08:27 +0000

    Deploying to gh-pages from @ trentkrogers/trentkrogers.github.io@5b122b89ed22c9b867ed4a9e8544dee7d92cba61 🚀

--- _next/static/PGFu8tDld4kVORMZD-E_Z/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-6872b199b781fb3e.js"],"/_error":["static/chunks/pages/_error-8e848193de3c139c.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
